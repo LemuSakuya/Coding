23ff6f14645aa6cff1137a7141bfec6ff5266db1
LemuSakuya <lemusakuya@gmail.com>
2025-02-24 12:37:27 +0800

    The Fifth Save

---
+++ PythonProject/v1.py
@@ -0,0 +1,14 @@
+glass1 = "tempessimo"
+glass2 = "factura ray"
+temp = ""
+
+print (glass1)
+print (glass2)
+print (temp)
+
+temp = glass1
+glass1 = glass2
+glass2 = temp
+
+print ("the glass1 is:", glass1)
+print ("the glass2 is:", glass2)
