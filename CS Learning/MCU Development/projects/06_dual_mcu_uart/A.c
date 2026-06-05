060476eae32d720ad3b754079714b2b08cbf325c
LemuSakuya <lemusakuya@gmail.com>
2026-06-05 21:06:13 +0800

    Upadate new MCU Code

--- CS Learning/MCU Development/projects/06_dual_mcu_uart/A.c
+++ CS Learning/MCU Development/projects/06_dual_mcu_uart/A.c
@@ -1,18 +1,15 @@
 /**
  * Experiment 06: Dual-MCU UART - Machine A
  *
- * Function according to the schematic:
- *   1. K1 on Machine A sends a command to Machine B.
- *   2. Machine A receives a digit from Machine B and displays it on the
- *      common-cathode 7-segment display connected to P0.
+ * Task:
+ *   Machine A receives characters '0'~'4' from Machine B at 9600 bit/s,
+ *   then displays the received number on the 7-segment display by table lookup.
  *
- * Crystal: 11.0592MHz, Baud: 9600
+ * Crystal: 11.0592MHz
  *
  * Hardware:
  *   P0.0~P0.7 -> 7-segment a,b,c,d,e,f,g,dp through pull-up resistor pack
- *   P1.7      -> K1 button, active low
  *   P3.0/RXD  -> Machine B P3.1/TXD
- *   P3.1/TXD  -> Machine B P3.0/RXD
  *   GND       -> common with Machine B
  */
 
@@ -21,61 +18,28 @@
 #define uint unsigned int
 #define uchar unsigned char
 
-#define CMD_BLINK 'L'
-
-sbit KEY_A = P1^7;
-
 /* Common-cathode 7-segment code: bit0~bit7 = a,b,c,d,e,f,g,dp */
 uchar code seg_table[] = {
     0x3f,  /* 0 */
     0x06,  /* 1 */
     0x5b,  /* 2 */
     0x4f,  /* 3 */
-    0x66,  /* 4 */
-    0x6d,  /* 5 */
-    0x7d,  /* 6 */
-    0x07,  /* 7 */
-    0x7f,  /* 8 */
-    0x6f   /* 9 */
+    0x66   /* 4 */
 };
 
 uchar g_display_num = 0;
 bit g_display_update = 0;
 
-void delay_ms(uint ms)
-{
-    uint i;
-    uchar j;
-
-    for (i = 0; i < ms; i++) {
-        for (j = 0; j < 120; j++) {
-            ;
-        }
-    }
-}
-
 void uart_init(void)
 {
-    SCON = 0x50;   /* mode 1, REN=1 */
+    SCON = 0x50;   /* serial mode 1, REN=1 */
     TMOD &= 0x0f;
-    TMOD |= 0x20;  /* Timer1 mode 2 */
-    TH1 = 0xfd;    /* 9600bps @ 11.0592MHz */
+    TMOD |= 0x20;  /* Timer1 mode 2, 8-bit auto reload */
+    TH1 = 0xfd;    /* 9600 bit/s @ 11.0592MHz */
     TL1 = 0xfd;
     TR1 = 1;
     ES = 1;
     EA = 1;
-}
-
-void uart_send(uchar dat)
-{
-    ES = 0;        /* avoid TI causing a serial interrupt while polling */
-    TI = 0;
-    SBUF = dat;
-    while (!TI) {
-        ;
-    }
-    TI = 0;
-    ES = 1;
 }
 
 void uart_isr(void) interrupt 4
@@ -86,7 +50,7 @@ void uart_isr(void) interrupt 4
         RI = 0;
         dat = SBUF;
 
-        if (dat >= '0' && dat <= '9') {
+        if (dat >= '0' && dat <= '4') {
             g_display_num = dat - '0';
             g_display_update = 1;
         }
@@ -100,25 +64,12 @@ void uart_isr(void) interrupt 4
 void main(void)
 {
     uart_init();
-    KEY_A = 1;             /* release P1.7 for input */
-    P0 = seg_table[0];     /* default display 0 */
+    P0 = seg_table[0];
 
     while (1) {
         if (g_display_update) {
             g_display_update = 0;
             P0 = seg_table[g_display_num];
-        }
-
-        if (KEY_A == 0) {
-            delay_ms(10);
-            if (KEY_A == 0) {
-                uart_send(CMD_BLINK);
-            }
-
-            while (KEY_A == 0) {
-                ;
-            }
-            delay_ms(10);
         }
     }
 }
--- CS Learning/MCU Development/projects/06_dual_mcu_uart/B.c
+++ CS Learning/MCU Development/projects/06_dual_mcu_uart/B.c
@@ -1,16 +1,14 @@
 /**
  * Experiment 06: Dual-MCU UART - Machine B
  *
- * Function according to the schematic:
- *   1. K2 on Machine B sends one digit to Machine A, then Machine A displays it.
- *   2. Machine B receives a command from Machine A and blinks its LEDs.
+ * Task:
+ *   Each time the button on P1.7 is pressed, Machine B sends one character
+ *   from '0' to '4'. After '4', it returns to '0'.
  *
- * Crystal: 11.0592MHz, Baud: 9600
+ * Crystal: 11.0592MHz, Baud: 9600 bit/s
  *
  * Hardware:
- *   P1.0/P1.3 -> LEDs through 220R to VCC, active low
- *   P1.7      -> K2 button, active low
- *   P3.0/RXD  -> Machine A P3.1/TXD
+ *   P1.7      -> button, active low
  *   P3.1/TXD  -> Machine A P3.0/RXD
  *   GND       -> common with Machine A
  */
@@ -20,14 +18,9 @@
 #define uint unsigned int
 #define uchar unsigned char
 
-#define CMD_BLINK 'L'
-
-sbit LED1 = P1^0;
-sbit LED2 = P1^3;
 sbit KEY_B = P1^7;
 
 uchar g_send_num = 0;
-bit g_blink_request = 0;
 
 void delay_ms(uint ms)
 {
@@ -43,80 +36,37 @@ void delay_ms(uint ms)
 
 void uart_init(void)
 {
-    SCON = 0x50;   /* mode 1, REN=1 */
+    SCON = 0x40;   /* serial mode 1, transmit only */
     TMOD &= 0x0f;
-    TMOD |= 0x20;  /* Timer1 mode 2 */
-    TH1 = 0xfd;    /* 9600bps @ 11.0592MHz */
+    TMOD |= 0x20;  /* Timer1 mode 2, 8-bit auto reload */
+    TH1 = 0xfd;    /* 9600 bit/s @ 11.0592MHz */
     TL1 = 0xfd;
     TR1 = 1;
-    ES = 1;
-    EA = 1;
 }
 
 void uart_send(uchar dat)
 {
-    ES = 0;        /* avoid TI causing a serial interrupt while polling */
     TI = 0;
     SBUF = dat;
     while (!TI) {
         ;
     }
     TI = 0;
-    ES = 1;
-}
-
-void blink_leds(void)
-{
-    uchar i;
-
-    for (i = 0; i < 3; i++) {
-        LED1 = 0;
-        LED2 = 0;
-        delay_ms(200);
-        LED1 = 1;
-        LED2 = 1;
-        delay_ms(200);
-    }
-}
-
-void uart_isr(void) interrupt 4
-{
-    uchar dat;
-
-    if (RI) {
-        RI = 0;
-        dat = SBUF;
-
-        if (dat == CMD_BLINK) {
-            g_blink_request = 1;
-        }
-    }
-
-    if (TI) {
-        TI = 0;
-    }
 }
 
 void main(void)
 {
     uart_init();
-    LED1 = 1;      /* LED off, active low */
-    LED2 = 1;
     KEY_B = 1;     /* release P1.7 for input */
 
     while (1) {
-        if (g_blink_request) {
-            g_blink_request = 0;
-            blink_leds();
-        }
-
         if (KEY_B == 0) {
             delay_ms(10);
             if (KEY_B == 0) {
                 uart_send('0' + g_send_num);
 
                 g_send_num++;
-                if (g_send_num > 9) {
+                if (g_send_num > 4) {
                     g_send_num = 0;
                 }
             }
